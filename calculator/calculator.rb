c27b03676f93204c21a755b139a0a5434dff67e1
Nalipp <nateplipp@gmail.com>
2016-06-09 18:18:23 +0900

    refactor calculator

--- calculator/calculator.rb
+++ calculator/calculator.rb
@@ -1,24 +1,86 @@
-puts "Welcome to Calculator"
-puts "What's the first number?"
-num1 = gets.chomp.to_i
-puts "What operstion would you like to preform? 1)add 2)subtract 3)multiply 4)divide"
-opp = gets.chomp
-puts opp
-puts "What's the second number?"
-num2 = gets.chomp.to_i
+def prompt(message)
+  puts "=> #{message}"
+end
 
-def calculator(num1, opp, num2)
-  if opp == "1"
-    num1 + num2
-  elsif opp == "2"
-    num1 - num2
-  elsif opp == "3"
-    num1 * num2
+def valid_number?(num)
+  num != 0
+end
+
+def operation_to_message(op)
+  op == '1' ? 'adding' : op == '2' ? 'subtracting' : op == '3' ? 'multiplying' : 'dividing'
+end
+
+prompt "Welcome to Calculator!, Enter your name:"
+
+name = ''
+loop do
+  name = gets.chomp
+  if name.empty?
+    prompt("Make sure you provide your name")
   else
-    num1.to_f / num2.to_f
+    prompt "Welcome #{name}"
+    break
   end
 end
 
-answer = calculator(num1, opp, num2)
+loop do
+  num1 = ''
+  loop do
+    prompt "What's the first number?"
+    num1 = gets.chomp.to_i
+    if valid_number?(num1)
+      break
+    else
+      prompt "Hmm... that dosen't look like a valid number"
+    end
+  end
 
-puts "The answer is #{answer}"
+  num2 = ''
+  loop do
+    prompt "What's the second number?"
+    num2 = gets.chomp.to_i
+    if valid_number?(num2)
+      break
+    else
+      prompt "Hmm... that dosen't look like a valid number"
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operstion would you like to preform?
+    1)add
+    2)subtract
+    3)multiply
+    4)divide
+  MSG
+
+  prompt operator_prompt
+  operator = ''
+  loop do
+    operator = gets.chomp
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt "Please choose 1,2,3 or 4"
+    end
+  end
+
+  prompt "#{operation_to_message(operator)} the two numbers..."
+
+  result = case operator
+          when "1"
+            num1 + num2
+          when "2"
+            num1 - num2
+          when "3"
+            num1 * num2
+          else
+            num1.to_f / num2.to_f
+          end
+
+  prompt "The answer is #{result}"
+
+  prompt "Do you want to preform another opperation? (y/n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with? 'y'
+end
