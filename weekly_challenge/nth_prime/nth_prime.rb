556cc176323c4797b7ca01dca650e42aef2f5030
Nalipp <nateplipp@gmail.com>
2016-07-17 21:19:10 +0900

    weekly exercises

---
+++ weekly_challenge/nth_prime/nth_prime.rb
@@ -0,0 +1,18 @@
+require_relative 'errors'
+
+class Prime
+  def self.nth(num)
+    raise ArgumentError, 'number must be greater than zero' if num < 1
+    index = 2
+    return_arr = []
+    while return_arr.length < num
+      return_arr << index if prime?(index)
+      index += 1
+    end
+    return_arr.last
+  end
+
+  def self.prime?(num)
+    (2..Math.sqrt(num)).each { |divisor| return false if num % divisor == 0 }
+  end
+end
