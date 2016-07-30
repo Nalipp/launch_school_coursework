e82ab985d36fdf27e55cf5a38b09159cf41e4454
Nalipp <nateplipp@gmail.com>
2016-07-30 19:42:23 +0900

    re-factor echo

--- web_development/echo_server/echo.rb
+++ web_development/echo_server/echo.rb
@@ -2,16 +2,16 @@ require "socket"
 require "Time"
 
 server = TCPServer.new("localhost", 3006)
-def dice(sides)
-  rand(sides) + 1
-end
 
-def dice_rolls(num, sides)
-  return_arr = []
-  num.times do
-    return_arr << dice(sides)
+def parse_request(request_line)
+  http_method, path_and_parameters, http = request_line.split
+  path, params = path_and_parameters.split('$')
+
+  params = params.split('&').each_with_object({}) do |pair, hash|
+    key, value = pair.split('=')
+    hash[key] = value
   end
-  return_arr
+  [http_method, path, params]
 end
 
 loop do
@@ -20,14 +20,19 @@ loop do
   request_line = client.gets
   client.puts request_line
 
-  params = Hash.new
-  request_line.split[1].gsub(/[^a-z=0-9&]/, '').split('&').each do |request|
-    params["#{request.split('=')[0]}"] = "#{request.split('=')[1]}"
+  http_method, path, params = parse_request(request_line)
+
+  client.puts http_method
+  client.puts path
+  client.puts params
+
+  rolls = params['rolls'].to_i
+  sides = params['sides'].to_i
+
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts roll
   end
-  
-  rolls = params["rolls"].to_i
-  sides = params["sides"].to_i
-  client.puts dice_rolls(rolls, sides)
 
   client.close
 end
