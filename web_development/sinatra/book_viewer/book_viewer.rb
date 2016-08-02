7d30994cc246f4bd11b4ca21264218aabc84a2f1
Nalipp <nateplipp@gmail.com>
2016-08-02 14:18:59 +0900

    helpers first attempt

--- web_development/sinatra/book_viewer/book_viewer.rb
+++ web_development/sinatra/book_viewer/book_viewer.rb
@@ -2,15 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-get "/" do
+before do
   @contents = File.readlines "data/toc.txt"
+end
+
+helpers do
+  def in_paragraphs(text)
+    copy = text.insert(0, '<p>')
+    copy = copy.gsub("\n\n", "</p> <p>")
+    copy << "</p>"
+  end
+end
+
+get "/" do
   @title = "Sherlock Homes"
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines "data/toc.txt"
-  @chapter = File.read "data/chp1.txt"
+get "/chapters/:number" do
+  number = params[:number]
+
+  chapter_title = @contents[number.to_i - 1]
+  @title = "Chapter #{number} : #{chapter_title}"
+  @chapter = File.read("data/chp#{number}.txt")
   erb :chapter
 end
