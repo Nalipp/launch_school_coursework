05a30e1c49460df545537e2afe62c20e5f1866da
Nalipp <nateplipp@gmail.com>
2016-08-17 17:04:49 +0900

    test accept money method attempt

--- exercises/course_130/medium/classes_to_test/cash_register_test.rb
+++ exercises/course_130/medium/classes_to_test/cash_register_test.rb
@@ -4,6 +4,11 @@ require_relative 'transaction'
 
 class CashRegisterTest < MiniTest::Test
   def setup
-    @test = cash_register.new(50)
+    @cash_register = CashRegister.new(5000)
+    @transaction = Transaction.new(100)
+  end
+
+  def test_accept_money
+    assert_equal 5000, @cash_register.accept_money(@transaction)
   end
 end
