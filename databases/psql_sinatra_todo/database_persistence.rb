a049dfe9ef2ffc543da668b7ed554ea5988c8bbe
Nalipp <nateplipp@gmail.com>
2016-09-29 07:38:13 +0900

    find list/todos functionality

--- databases/psql_sinatra_todo/database_persistence.rb
+++ databases/psql_sinatra_todo/database_persistence.rb
@@ -17,7 +17,10 @@ class DatabasePersistence
     result = query(sql, id)
 
     tuple = result.first
-    { id: tuple["id"], name: tuple["name"], todos: [] }
+
+    list_id = tuple["id"].to_i
+    todos = find_todos_for_list(id)
+    { id: list_id, name: tuple["name"], todos: todos }
   end
 
   def all_lists
@@ -25,7 +28,9 @@ class DatabasePersistence
     result = query(sql)
 
     result.map do |tuple|
-      { id: tuple["id"], name: tuple["name"], todos: [] }
+      list_id = tuple["id"].to_i
+      todos = find_todos_for_list(list_id)
+      {id: list_id, name: tuple["name"], todos: todos}
     end
   end
 
@@ -64,5 +69,18 @@ class DatabasePersistence
   def mark_all_todos_as_completed(list_id)
     # list = find_list(list_id)
     # list[:todos].each { |todo| todo[:completed] = true }
+  end
+
+  private
+
+  def find_todos_for_list(list_id)
+    sql = "SELECT * FROM todos WHERE list_id = $1"
+    todos_result = query(sql, list_id)
+
+    todos_result.map do |todo_tuple|
+      { id: todo_tuple["id"].to_i,
+        name: todo_tuple["name"],
+        completed: todo_tuple["completed"] == "t"}
+    end
   end
 end
