8b6a1a779f06e153b192d994170e9e0d245822cf
Nalipp <nateplipp@gmail.com>
2016-09-28 17:00:45 +0900

    add sinatra logger sql

--- databases/psql_sinatra_todo/database_persistence.rb
+++ databases/psql_sinatra_todo/database_persistence.rb
@@ -2,12 +2,13 @@ require "pry"
 require "pg"
 
 class DatabasePersistence
-  def initialize
+  def initialize(logger)
     @db = PG.connect(dbname: "todos")
+    @logger = logger
   end
 
   def query(statement, *params)
-    puts "#{statement}: #{params}"
+    @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
   end
 
